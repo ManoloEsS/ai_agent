910b6d78f655a2be264188d424391b6ed767443b
ManoloEsS <manolo.estrada.s@gmail.com name = ManoloEsS>
2025-06-23 08:56:07 -0600

    added all functions

---
+++ functions/get_file_content.py
@@ -0,0 +1,25 @@
+import os
+
+from config import MAX_CHARS
+
+
+def get_file_content(working_directory, file_path):
+    absolute_working = os.path.abspath(working_directory)
+    target_file = os.path.abspath(os.path.join(working_directory, file_path))
+
+    if not target_file.startswith(absolute_working):
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    if not os.path.isfile(target_file):
+        return f'Error: "File not found or is not a regular file: "{file_path}"'
+
+    try:
+        with open(target_file, "r") as f:
+            file_content_string = f.read(MAX_CHARS)
+            more = f.read(1)
+            if more:
+                file_content_string += (
+                    f'[...File "{file_path}" truncated at 10000 characters]'
+                )
+            return file_content_string
+    except Exception as e:
+        return f'Error reading file "{file_path}": {e}'
---
+++ functions/run_python_file.py
@@ -0,0 +1,32 @@
+import os
+import subprocess
+
+
+def run_python_file(working_directory, file_path, args=None):
+    absolute_working = os.path.abspath(working_directory)
+    target_file = os.path.abspath(os.path.join(working_directory, file_path))
+
+    if not target_file.startswith(absolute_working):
+        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+
+    if not os.path.exists(target_file):
+        return f'Error: File "{file_path}" not found.'
+
+    if not target_file.endswith(".py"):
+        return f'Error: "{file_path}" is not a Python file.'
+
+    try:
+        commands = ["python3", target_file]
+        if args:
+            commands.extend(args)
+        completed = subprocess.run(
+            commands, capture_output=True, timeout=30, text=True, cwd=absolute_working
+        )
+        if not completed:
+            return "No output produced."
+        output = f"STDOUT: {completed.stdout}\nSTDERR: {completed.stderr}\n"
+        if completed.returncode != 0:
+            output += f"Process exited with code {completed.returncode}\n"
+        return output
+    except Exception as e:
+        return f"Error: executing Python file: {e}"
